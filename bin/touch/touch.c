7b6c9d7dc55401cfe79ce235a939ae269a416042
powerinstashoot <41297578+powerinstashoot@users.noreply.github.com>
2021-03-24 20:41:56 +0100

    New version of touch command
    I have updated it because Edurne said that we should create this command with open system call instead of creat. Hope you enjoy it.

--- bin/touch/touch.c
+++ bin/touch/touch.c
@@ -4,8 +4,8 @@
  *
  * @brief The command used to create items.
  *
- * @version 0.1
- * @date 2021-03-23
+ * @version 0.2
+ * @date 2021-03-24
  *
  * @copyright Copyright (c) 2021
  */
@@ -17,13 +17,22 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 
 /**
-* @brief This void method will call to the open function.
+* @brief This void method will call to the open function. In case of error, the open system call will return -1 and an error message will appear.
 * @param f_name that will be the wanted file name to be created.
+* @param flags that will be the flags of the system call.
+* @param modes that will be the permissions of the file created.
 */
- void _touch(char *f_name, int RDWR){
-     creat(f_name, RDWR); //The open function will be executed always to read and write.
+ void _touch(char *f_name, char flags, int modes){
+    int fd;
+    fd = open(f_name, flags, modes);
+    if(fd==-1){
+        printf(" touch was failed - errno =(%d) \n",errno);
+    }else{
+        printf(" touch executed successfully\n");
+    }
  }
 
 /**
@@ -39,7 +48,12 @@
         write(2, "Usage: touch file_name\n", 22);
 
      }else if(argc==2){
-        _touch(argv[1], O_RDWR);
+        //Explanation of modes:
+        //S_IRWXU: 00700 user (the file owner) has read, write and execute permission.
+        //S_IRUSR: 00400 user has read permission.
+        //S_IRGRP: 00040 group has read permission00040 group has read permission.
+        //S_IROTH: 00004 others have read permission.
+        _touch(argv[1], O_CREAT, S_IRWXU|S_IRUSR|S_IRGRP|S_IROTH);
      }
      return 0;
  }
