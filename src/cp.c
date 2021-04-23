9afd4cd2a33b357b2ba8d05d1eb8155353833e05
Mikel <powerinstashoot@gmail.com>
2021-04-23 12:00:55 +0200

    cp command updated

--- src/cp.c
+++ src/cp.c
@@ -1,38 +1,41 @@
 /**
  * @file cp.c
- * @author ...
- * @brief ...
- * @version 0.1
- * @date 2021-03-30
- * 
+ * @author Mikel Aristu
+ * @brief cp command to copy files
+ * @version 0.2
+ * @date 2021-04-23
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
 #include "headers/cp.h"
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    char buffer[1024];
-    int files[2];
-    ssize_t count;
+    int firstFD, endFD, oread, owrite;
+    char *buffer[1024];
 
-    if (argc < 3)
-        return -1;
+    if (argc < 3 || argc >3){
+        printf("Usage:\n cp file new_file\n");
+   }else{
 
-    files[0] = open(argv[1], O_RDONLY);
-    if (files[0] == -1) 
-        return -1;
-    
-    files[1] = open(argv[2], O_WRONLY | O_CREAT | S_IRWXU | S_IRUSR  | S_IRGRP | S_IROTH);
-    if (files[1] == -1) 
-    {
-        close(files[0]);
-        return -1;
+    firstFD = open(argv[1], O_RDONLY);
+    if (firstFD == -1){
+        write(2,"Error opening the file.",strlen("Error opening the file."));
     }
 
-    while ((count = read(files[0], buffer, sizeof(buffer))) != 0)
-        write(files[1], buffer, count);
+    endFD = open(argv[2], O_RDWR | O_CREAT, S_IWUSR | S_IRUSR  | S_IRGRP | S_IROTH);
+    if (endFD == -1)
+    {
+        printf("Error opening file %s errno = %d", argv[2], errno);
+        exit(EXIT_FAILURE);
+    }
 
+    while ((oread = read(firstFD, buffer, sizeof(buffer))) > 0)
+        if(write(endFD,buffer,oread)!=oread){
+           printf("Error in writing data to %s",argv[2]);
+        }
+    }
     return 0;
 }
